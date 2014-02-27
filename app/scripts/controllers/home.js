9394666d518ecb32d6ff5abeff118b4b58e2238a
Ben Tesser <btesser@gmail.com>
2014-02-27 08:48:08 -0500

    controllers

---
+++ app/scripts/controllers/home.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('youtubemobiApp')
+  .controller('HomeCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/manage-videos.js
@@ -0,0 +1,38 @@
+'use strict';
+
+angular.module('youtubemobiApp')
+  .controller('ManageVideosCtrl', function ($scope, Suits, Cards, $stateParams) {
+    $scope.cards = Cards;
+    $scope.suits = Suits;
+    angular.forEach(Suits, function(suit){
+      $scope.videos[suit.name] = {};
+      if(suit.name.toLowerCase() === $stateParams.suit.toLowerCase()) $scope.suit = suit;
+      angular.forEach(Cards, function(card){
+      if(suit.name.toLowerCase() === $stateParams.suit.toLowerCase()) $scope.card = card;
+        $scope.videos[suit.name][card.abbreviation] = {
+          url: null,
+          namePosition: null
+        };
+      });
+    });
+    $scope.card = Cards[0];
+    $scope.suit = Suits[0];
+    $scope.videos = {};
+    $scope.next = function next() {
+      $scope.card = Cards[Cards.indexOf($scope.card)+1];
+      if(typeof $scope.card === 'undefined'){
+        $scope.card = Cards[0];
+        $scope.suit = Suits[Suits.indexOf($scope.suit)+1];
+        if(typeof $scope.suit === 'undefined') $scope.suit = Suits[0];
+      }
+    };
+    $scope.previous = function next() {
+      $scope.card = Cards[Cards.indexOf($scope.card)-1];
+      if(typeof $scope.card === 'undefined'){
+        $scope.card = Cards[Cards.length-1];
+        $scope.suit = Suits[Suits.indexOf($scope.suit)-1];
+        if(typeof $scope.suit === 'undefined') $scope.suit = Suits[Suits.length-1];
+      }
+    };
+
+  });
